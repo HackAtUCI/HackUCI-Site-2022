54a268566254814f24e9f555c3a0f704c27b23fd
Lasse Nordahl <lasseanordahl@gmail.com>
2019-05-27 11:31:14 -0700

    updated Ryans commit. Added information to the index.js file for views and moved routing into app.js to use appwide css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import './App.scss';
 
-import { FeedbackForm } from './app/components/FeedbackHooks/FeedbackForm'
-
-import Button from 'react-bootstrap/Button';
+import AppNavbar from "./app/components/app-navbar/AppNavbar";
+import Application from "./app/views/application/Application";
+import Confirmation from "./app/views/confirmation/Confirmation";
+import Dashboard from "./app/views/dashboard/Dashboard";
+import Home from "./app/views/home/Home";
+import LiveExpo from "./app/views/live-expo/LiveExpo";
+import LiveSchedule from "./app/views/live-schedule/LiveSchedule";
+import Login from "./app/views/login/Login";
+import ResetPassword from "./app/views/reset-password/ResetPassword";
+import Sponsorship from "./app/views/sponsorship/Sponsorship";
+import NotFound from "./app/views/not-found/NotFound";
 
 function App() {
   return (
     <div className="App">
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-        <h3>
-          This is the HackUCI React Port Website
-        </h3>
-        <FeedbackForm />
-      </div>
+      <Router>
+        <div>
+          <AppNavbar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/application" component={Application} />
+            <Route exact path="/confirmation" component={Confirmation} />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/live-expo" component={LiveExpo} />
+       p     <Route exact path="/live-schedule" component={LiveSchedule} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/reset-password" component={ResetPassword} />
+            <Route exact path="/sponsors" component={Sponsorship} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
