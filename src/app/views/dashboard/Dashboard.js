0260a75572dce1f133f3e7d7480542e18a56bd63
Ryan Luu <ryanluu12345@gmail.com>
2019-05-27 15:56:35 -0700

    Added pre-commit hook for prettier linting

--- src/app/views/dashboard/Dashboard.js
+++ src/app/views/dashboard/Dashboard.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Dashboard(props) {
-  return <div>Dashboard page</div>;
+  return (
+    <div>
+      <h1>Dashboard page</h1>
+    </div>
+  );
 }
