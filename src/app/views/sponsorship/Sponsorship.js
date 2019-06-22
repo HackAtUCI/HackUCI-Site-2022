3d8b649d1e45987cb0fdfc9308d27d90f1aaf0ae
Theja Krishna <takrishn@uci.edu>
2019-06-22 00:32:50 -0700

    forgot to add extension to import

--- src/app/views/sponsorship/Sponsorship.js
+++ src/app/views/sponsorship/Sponsorship.js
@@ -3,7 +3,7 @@ import React from "react";
 import Welcome from "./welcome/Welcome.js";
 import MoreInfo from "./more-info/MoreInfo.js";
 import PastSponsors from "./past-sponsors/PastSponsors.js";
-import Recap from "./recap/Recap";
+import Recap from "./recap/Recap.js";
 
 import "./Sponsorship.scss";
 
