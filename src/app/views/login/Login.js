236323639b7bda56d4f3c204bdf683c127af08eb
takrishn <takrishn@uci.edu>
2019-07-07 13:22:29 -0700

    updated login validation

--- src/app/views/login/Login.js
+++ src/app/views/login/Login.js
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
 
 import AuthService from "../../../services/AuthService";
+import useForm from "../../../hooks/useForm";
+import { validation } from "../../../utils/validation.js";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { values, errors, setErrors, handleChange, handleSubmit } = useForm(
+    loginWithPassword,
+    validation.processLoginForm
+  );
 
-  function handleEmailInput(event) {
-    setEmail(event.target.value);
-  }
-
-  function handlePasswordInput(event) {
-    setPassword(event.target.value);
-  }
-
-  function handleSubmit() {
-    AuthService.loginWithPassword(email, password)
+  function loginWithPassword() {
+    AuthService.loginWithPassword(values.email, values.password)
       .then(response => {
-        console.log("successful");
+        // successful login
+        console.log(response);
       })
       .catch(err => {
-        console.log("error");
+        // login failed
+        setErrors({ networkError: err.message });
       });
   }
 
@@ -33,19 +31,34 @@ export default function Login() {
         <Form.Group controlId="email.ControlInput1">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            onChange={handleEmailInput}
+            name="email"
+            onChange={handleChange}
+            class={"form-control" + (errors.email ? " error" : "")}
+            value={values.email || ""}
             type="email"
             placeholder="foo@bar.edu"
           />
+          <div>
+            <p class="red">{errors.email}</p>
+          </div>
         </Form.Group>
         <Form.Group controlId="password.ControlInput2">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            onChange={handlePasswordInput}
+            name="password"
+            onChange={handleChange}
+            class={"form-control" + (errors.password ? " error" : "")}
+            value={values.password || ""}
             type="password"
-            placeholder="password"
+            placeholder="Password"
           />
+          <div>
+            <p class="red">{errors.password}</p>
+          </div>
         </Form.Group>
+        <div>
+          <p class="red">{errors.networkError}</p>
+        </div>
         <Button onClick={handleSubmit}>Submit</Button>
       </Form>
     </div>
