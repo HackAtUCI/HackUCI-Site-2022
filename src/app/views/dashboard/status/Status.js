92319f7cfe18738131573609f6809bd18aca745f
takrishn <takrishn@uci.edu>
2019-08-15 19:41:38 -0700

    resolved PR comments: added registrationDeadline and confirmationDeadline, fixed useEffect()

--- src/app/views/dashboard/status/Status.js
+++ src/app/views/dashboard/status/Status.js
@@ -1,48 +1,59 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import UserService from "../../../../services/UserService";
+
 import "./status.scss";
 import Button from "react-bootstrap/Button";
 
 function Status() {
+  // temporarily defaulted to old strings. will be updated with new mockups.
+  const [registrationDeadline, setRegistrationDeadline] = useState(
+    "Friday, January 25th 2019, 11:59 pm (PST)"
+  );
+  const [confirmationDeadline, setConfirmationDeadline] = useState(
+    "Saturday, January 19th 2019, 9:00 am (PST)"
+  );
+  // current status is initialized to unverified below the statuses constants
+
   // Constants for all the statuses (the text descriptions are temporary. status cases were taken from quill)
   // the status titles were modified to fit the design document
   const statuses = {
     unverified: {
       status: "UNVERIFIED",
-      text: "users have not verified the email address they registered with"
+      text: "Users have not verified the email address they registered with."
     },
     incompleteRegistrationOpen: {
       status: "INCOMPLETE",
       text:
-        "the user has not submitted their application, but the registration deadline has not passed"
+        "The user has not submitted their application, but the registration deadline has not passed."
     },
     incompleteRegistrationClosed: {
       status: "INCOMPLETE",
       text:
-        "the user has not submitted, but the registration deadline has passed"
+        "The user has not submitted, but the registration deadline has passed."
     },
     submittedRegistrationOpen: {
       status: "UNDER REVIEW",
-      text: "submitted registration open"
+      text: "Submitted registration open."
     },
     submittedRegistrationClosed: {
       status: "UNDER REVIEW",
-      text: "submitted registration closed"
+      text: "Submitted registration closed."
     },
     admittedUnconfirmed: {
       status: "ADMITTED",
       text:
-        "the user has been admitted to the event, but has not confirmed their attendance and submitted their confirmation form"
+        "The user has been admitted to the event, but has not confirmed their attendance and submitted their confirmation form."
     },
     admittedCofirmationDeadlinePassed: {
       status: "EXPIRED ADMISSION",
       text:
-        "the user has been admitted, but did not confirm their attendance before the deadline"
+        "The user has been admitted, but did not confirm their attendance before the deadline."
     },
     waitlisted: {
       status: "WAITLISTED",
-      text: "the user was not admitted to the event"
+      text: "The user was not admitted to the event."
     },
     confirmed: {
       status: "CONFIRMED",
@@ -51,17 +62,24 @@ function Status() {
     },
     declined: {
       status: "DECLINED ADMISSION",
-      text: "the user has been admitted, but will not be attending the event"
+      text: "The user has been admitted, but will not be attending the event."
     }
   };
 
-  // current status temporarily initialized to "confirmed" for testing purposes
-  // declared and initialized below "statuses" const until useEffect is completed
-  const [currentStatus, setCurrentStatus] = useState(statuses.confirmed);
+  // default value is set to unverified (underneath the statuses declaration)
+  const [currentStatus, setCurrentStatus] = useState(statuses.unverified);
 
+  // only rerender if the currentStatus, registrationDeadline, or confirmationDeadline has changed.
   useEffect(() => {
-    // TODO: get the user and update current status
-  });
+    UserService.getCurrentUser()
+      .then(response => {
+        // TODO: Update currentStatus, registrationDeadline, and confirmationDeadline
+        console.log(response);
+      })
+      .catch(err => {
+        console.log("service unavailable");
+      });
+  }, [currentStatus, registrationDeadline, confirmationDeadline]);
 
   function declineAdmission() {
     // TODO: decline admission call
@@ -74,12 +92,10 @@ function Status() {
       <div className="status-box">{currentStatus.status}</div>
       <div className="deadline-container">
         <p className="deadline">
-          <b>Registration Deadline:</b>&nbsp;Friday, January 25th 2019, 11:59 pm
-          (PST)
+          <b>Registration Deadline:</b>&nbsp;{registrationDeadline}
         </p>
         <p className="deadline">
-          <b>Confirmation Deadline:</b>&nbsp;Saturday, January 19th 2019, 9:00
-          am (PST)
+          <b>Confirmation Deadline:</b>&nbsp;{confirmationDeadline}
         </p>
       </div>
       <p className="status-text">{currentStatus.text}</p>
