2d3b5202ab27884da9e37bf51d8168705c1edfc5
Kevin <kevinfang28@gmail.com>
2019-10-20 13:18:56 -0700

    get rid of pointless console log

--- src/app/views/apply/Apply.js
+++ src/app/views/apply/Apply.js
@@ -78,10 +78,6 @@ export default function Apply(props) {
               });
           })
           .catch(err => {
-            const responseErrMsg = err.response
-              ? err.response.data.message
-              : err.message;
-            console.log(responseErrMsg);
             setshowStatus({
               showError: true,
               errorMessage: errorMessages.default
