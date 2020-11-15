4b013ca726ae8ae830762a68c9d19ed6de178dc1
Nathan Tang <nathanwt@uci.edu>
2020-11-15 09:37:05 -0800

    functional mobile friendly nav-bar

--- src/app/components/app-navbar/AppNavbar.js
+++ src/app/components/app-navbar/AppNavbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
@@ -6,55 +7,61 @@ import Hamburger from "../../../assets/images/ic_fluent_navigation_24_regular.sv
 
 import HackLogo from "../../../assets/images/white-anteater-logo-vectorized.svg";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import "./appNavbar.scss";
 
 export default function AppNavbar() {
+  const location = useLocation();
+  const [currentPath, setcurrentPath] = useState("");
+
+  useEffect(() => {
+    setcurrentPath(location.pathname);
+  }, [location]);
+
+  console.log(currentPath);
+
   return (
-    <>
-      <Navbar className="hack-navbar">
-        <Navbar.Brand>
-          <Link to="/">
-            <img src={HackLogo} alt="HackLogo" className="hack-navbar-logo" />
-          </Link>
-        </Navbar.Brand>
+    <Navbar className="hack-navbar" expand="lg">
+      <Navbar.Brand>
+        <Link to="/">
+          <img src={HackLogo} alt="HackLogo" className="hack-navbar-logo" />
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle
+        className="hack-nav-hamburger"
+        aria-controls="iner-navbar-nav"
+      />
+      <Navbar.Collapse>
         <Nav className="ml-auto">
           <Nav.Link className="hack-nav-item nav-non-button-item">
-            <Link to="/">Home</Link>
+            <Link to="/" className={currentPath === "/" && "selected"}>
+              Home
+            </Link>
           </Nav.Link>
           <Nav.Link className="hack-nav-item nav-non-button-item">
-            <Link to="/sponsors">Sponsor Us</Link>
+            <Link
+              to="/sponsors"
+              className={currentPath.includes("sponsors") && "selected"}
+            >
+              Sponsor Us
+            </Link>
           </Nav.Link>
           <Nav.Link className="hack-nav-item nav-non-button-item">
-            <Link to="/dashboard">Dashboard</Link>
+            <Link
+              to="/dashboard"
+              className={currentPath.includes("dashboard") && "selected"}
+            >
+              Dashboard
+            </Link>
           </Nav.Link>
-          <Nav.Link>
-            <Button className="hack-nav-item hack-button">
+          <Nav.Link className="hack-nav-item">
+            <Button className="hack-button">
               <Link to="/login">Login</Link>
             </Button>
           </Nav.Link>
-          <Button className="hack-nav-hamburger">
-            <img src={Hamburger} alt="Menu" />
-          </Button>
         </Nav>
-      </Navbar>
-      <div className="hack-nav-mobile-menu">
-        <Nav.Link className="nav-non-button-item">
-          <Link to="/">Home</Link>
-        </Nav.Link>
-        <Nav.Link className="nav-non-button-item">
-          <Link to="/sponsors">Sponsor Us</Link>
-        </Nav.Link>
-        <Nav.Link className="nav-non-button-item">
-          <Link to="/dashboard">Dashboard</Link>
-        </Nav.Link>
-        <Nav.Link>
-          <Button className="hack-button">
-            <Link to="/login">Login</Link>
-          </Button>
-        </Nav.Link>
-      </div>
-    </>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
