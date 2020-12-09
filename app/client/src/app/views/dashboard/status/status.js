192bd374182ee6e8e2ad8bb577d9524e662e0e85
Nathan Tang <nate.tang2@gmail.com>
2020-12-09 15:04:24 -0800

    laid down foundation for TODOs List, still can be expanded upon.

--- app/client/src/app/views/dashboard/status/status.js
+++ app/client/src/app/views/dashboard/status/status.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import statuses from "../../../../globals/statuses";
+import HackCheckboxChecked from "../../../../assets/images/checkbox_checked.svg";
+import HackCheckboxUnchecked from "../../../../assets/images/checkbox_unchecked.svg";
 
 import Button from "react-bootstrap/Button";
 
@@ -18,7 +20,74 @@ function Status(props) {
       <p className="status-header">Your Current Status</p>
       <div className="status-box">{dashboardUser.status.status}</div>
       <p className="status-text">{dashboardUser.status.text}</p>
-      <p className="status-header">TODOs</p>
+      <p className="status-todo">TODOs</p>
+      <div className="hack-dashboard-todo-container">
+        {/*would like to insert waiver stuff here but need data from HelloSign or something*/}
+        {/*baseline set-up for TODOs, there's probably a better way to do this with arrays, but I guess this is clearer to read*/}
+        <ul>
+          {(dashboardUser.status.status === undefined ||
+            dashboardUser.status.status ===
+              statuses.incompleteRegistrationClosed ||
+            dashboardUser.status.status ===
+              statuses.incompleteRegistrationOpen) && (
+            <li>
+              <img
+                className="hack-dashboard-checkbox"
+                src={HackCheckboxUnchecked}
+                alt="apply to hackUCI unchecked"
+              />
+              Apply to HackUCI
+            </li>
+          )}
+          {dashboardUser.status.status === statuses.unverified && (
+            <li>
+              <img
+                className="hack-dashboard-checkbox"
+                src={HackCheckboxUnchecked}
+                alt="verify email"
+              />
+              Verify Email
+            </li>
+          )}
+          {(dashboardUser.status.status === statuses.admittedUnconfirmed ||
+            dashboardUser.status.status ===
+              statuses.admittedConfirmationDeadlinePassed ||
+            dashboardUser.status.status ===
+              statuses.submittedRegistrationClosed ||
+            dashboardUser.status.status ===
+              statuses.submittedRegistrationOpen ||
+            dashboardUser.status.status === statuses.confirmed) && (
+            <li>
+              <img
+                className="hack-dashboard-checkbox"
+                src={HackCheckboxChecked}
+                alt="apply to hackUCI checked"
+              />
+              Apply to HackUCI
+            </li>
+          )}
+          {dashboardUser.status.status === statuses.admittedUnconfirmed && (
+            <li>
+              <img
+                className="hack-dashboard-checkbox"
+                src={HackCheckboxUnchecked}
+                alt="confirm registration"
+              />
+              Confirm your registration
+            </li>
+          )}
+          {dashboardUser.status.status === statuses.confirmed && (
+            <li>
+              <img
+                className="hack-dashboard-checkbox"
+                src={HackCheckboxChecked}
+                alt="confirm registration"
+              />
+              Confirm your registration
+            </li>
+          )}
+        </ul>
+      </div>
       {/*TODO: TODO*/}
       <div className="deadline-container">
         <p className="deadline">
