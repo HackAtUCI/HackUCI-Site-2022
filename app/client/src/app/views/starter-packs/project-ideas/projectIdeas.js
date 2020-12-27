aaadce754ce94a75cef3d63f93cd968e63c8ac20
Lasse Nordahl <lasseanordahl@gmail.com>
2020-12-27 10:32:33 -0800

    new route

---
+++ app/client/src/app/views/starter-packs/project-ideas/projectIdeas.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./projectIdeas.scss";
+
+function projectIdeas(props) {
+  return (
+    <React.Fragment>
+      <div className="project-ideas">starter packs</div>
+    </React.Fragment>
+  );
+}
+
+export default projectIdeas;
