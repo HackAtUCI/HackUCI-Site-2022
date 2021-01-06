39b4ebd386f87608ebc2863e6d6524e6a4c751ab
Lasse Nordahl <lasseanordahl@gmail.com>
2021-01-06 13:34:40 -0800

    updates

--- app/client/src/app/views/starter-packs/starterPacks.js
+++ app/client/src/app/views/starter-packs/starterPacks.js
@@ -2,12 +2,44 @@ import React from "react";
 
 import "./starterPacks.scss";
 
+import { Fireflies } from "app/components";
+import { starterPackData } from "./../../../assets/data/starter-pack-info.js";
+
 import ProjectIdeas from "./project-ideas/projectIdeas";
 
 function StarterPacks(props) {
   return (
     <div className="starter-packs">
-      <ProjectIdeas></ProjectIdeas>
+      <Fireflies fireflyCount={30} />
+      <ProjectIdeas />
+      <h2 style={{ marginBottom: "2rem" }}>Starter Packs</h2>
+      {starterPackData.map(function(starterPack, index) {
+        return (
+          <div className="starter-pack-card" key={index}>
+            <div className="starter-pack-card-information">
+              <h3>{starterPack.name}</h3>
+              <p>{starterPack.description}</p>
+            </div>
+            <div className="starter-pack-card-links">
+              {starterPack.packs.map(function(pack, indexTwo) {
+                return (
+                  <a
+                    className="starter-pack-card-link"
+                    href={pack.link}
+                    target="_blank"
+                    key={indexTwo}
+                  >
+                    <div>
+                      <h4>{pack.name}</h4>
+                    </div>
+                  </a>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+      <div style={{ height: "5rem" }}></div>
     </div>
   );
 }
---
+++ app/client/src/assets/data/starter-pack-info.js
@@ -0,0 +1,80 @@
+let starterPackData = [
+  {
+    name: "User Interface",
+    description: "Build a frontend you can interact with!",
+    packs: [
+      {
+        name: "ZotHacks React Frontend",
+        link: "https://github.com/hackuci/zothacks-2020-frontend"
+      },
+      {
+        name: "iOS App Development Workshop",
+        link:
+          "https://drive.google.com/file/d/1ArpHjIG1f_5fogtNDPk56dt6QyDKf0re/view"
+      },
+      {
+        name: "Android Zothacks Workshop",
+        link:
+          "https://drive.google.com/file/d/1IixnR7Lp__E-bocrmFbcdWm5q74zsKVI/view"
+      },
+      {
+        name: "ReactJS",
+        link: "https://reactjs.org/"
+      }
+    ]
+  },
+  {
+    name: "Backend",
+    description: "Build an API for data management and processing.",
+    packs: [
+      {
+        name: "ZotHacks Flask Backend",
+        link: "https://github.com/hackuci/zothacks-2020-backend"
+      },
+      {
+        name: "FastAPI",
+        link: "https://fastapi.tiangolo.com/"
+      }
+    ]
+  },
+  ,
+  {
+    name: "Gaming",
+    description: "Build the next chart topping indie game.",
+    packs: [
+      {
+        name: "Unity",
+        link: "https://docs.unity3d.com/Manual/index.html"
+      },
+      {
+        name: "Gadot",
+        link: "https://godotengine.org/"
+      },
+      {
+        name: "PyGame",
+        link: "https://www.pygame.org/wiki/GettingStarted"
+      }
+    ]
+  },
+  ,
+  {
+    name: "Tools",
+    description: "Gotta build your projects with something!",
+    packs: [
+      {
+        name: "Git",
+        link: "https://github.com/zpinto/learn-git-with-hack"
+      },
+      {
+        name: "Ngrok",
+        link: "https://ngrok.io"
+      },
+      {
+        name: "Postman",
+        link: "https://www.getpostman.com/"
+      }
+    ]
+  }
+];
+
+export { starterPackData };
