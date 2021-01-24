5be43eff59b0d6a2d3b4e81501e55de781cbdfee
Justin Yue <justinyue1643@gmail.com>
2021-01-24 09:29:02 -0800

    Live Expo css and autoplay added

--- app/client/src/app/views/live-expo/liveExpo.js
+++ app/client/src/app/views/live-expo/liveExpo.js
@@ -6,16 +6,16 @@ import "./liveExpo.scss";
 import { Fireflies } from "app/components/";
 
 export default function LiveExpo(props) {
-  // 60% desktop view
-  // mobile/ipad 80% 
+  
   return (
     <div className="live-expo">
       <Fireflies fireflyCount={30} />
-      <h2>Live Expo</h2>
-      <p>The Youtube stream on which {props.eventName ? props.eventName : "<enter event name>"} will be held.</p>
+      <h2 className = "live-expo-title">Live Expo</h2>
+      <p className = "live-expo-descrip">The Youtube stream on which {props.eventName ? props.eventName : "<enter event name>"} will be held.</p>
       <Youtube 
         className = "video-stream"
-        videoId = "eY3s4JVcqt0"
+        videoId = "7croXcvsn-c"
+        opts = {{playerVars: {autoplay: 1}}}
       />
     </div>
   );
