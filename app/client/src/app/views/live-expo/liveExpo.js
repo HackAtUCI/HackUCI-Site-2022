767b6dd4e84d8fd740dff8a99073ec2787594cb9
Lasse Nordahl <lasseanordahl@gmail.com>
2021-01-26 20:20:52 -0800

    added sponsor login modal to block unverified pages

--- app/client/src/app/views/live-expo/liveExpo.js
+++ app/client/src/app/views/live-expo/liveExpo.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Youtube from "react-youtube";
-import { Link } from "react-router-dom";
 
 import "./liveExpo.scss";
 
-import Modal from "react-modal";
+import Youtube from "react-youtube";
 import useAuth from "../../../hooks/useAuth";
-import { Fireflies } from "app/components/";
+import { Fireflies, SponsorLoginModal } from "app/components/";
 
 export default function LiveExpo(props) {
   const { isLoggedIn, resendVerificationEmail } = useAuth();
@@ -14,6 +12,7 @@ export default function LiveExpo(props) {
 
   useEffect(() => {
     if (!isLoggedIn) {
+      console.log("not logged in");
       setIsModalOpen(true);
     }
   }, []);
@@ -30,32 +29,6 @@ export default function LiveExpo(props) {
     }
   }
 
-  let styles = {
-    content: {
-      textAlign: "center",
-      backgroundColor: "rgba(0, 0, 0, 0.8)",
-      borderRadius: "1rem",
-      border: "0",
-      padding: "1rem",
-      width: "600px",
-      height: "200px",
-      position: "absolute",
-      marginLeft: "auto",
-      marginRight: "auto",
-      margin: "auto",
-      left: "0",
-      right: "0",
-      color: "white",
-      fontSize: "2rem",
-      fontFamily: "Catamaran",
-      display: "flex",
-      flexDirection: "column"
-    },
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.4)"
-    }
-  };
-
   return (
     <div className="live-expo">
       <Fireflies fireflyCount={30} />
@@ -68,66 +41,11 @@ export default function LiveExpo(props) {
         videoId="UEncaPrcn_U"
         opts={{ playerVars: { autoplay: 1 } }}
       />
-      <Modal
+      <SponsorLoginModal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
-        contentLabel="Example Modal"
-        shouldCloseOnOverlayClick={false}
-        style={styles}
-      >
-        <p>
-          Stage and Schedule views are only available if you are logged in or a
-          sponsor!
-        </p>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-          className="modal-button-container"
-        >
-          <Link
-            to="/login"
-            style={{
-              border: "3px solid #FED9A7",
-              width: "165px",
-              marginRight: "0.5rem",
-              height: "50px",
-              padding: "0.5rem",
-              borderRadius: "1rem",
-              borderCollapse: "separate",
-              display: "inline-block",
-              backgroundColor: "rgba(0, 0, 0, 0.8)",
-              fontSize: "1rem",
-              color: "#FED9A7",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
-            }}
-          >
-            Login
-          </Link>
-          <button
-            style={{
-              border: "3px solid #FED9A7",
-              width: "165px",
-              marginLeft: "0.5rem",
-              height: "50px",
-              padding: "0.5rem",
-              borderRadius: "1rem",
-              borderCollapse: "separate",
-              display: "inline-block",
-              backgroundColor: "rgba(0, 0, 0, 0.8)",
-              fontSize: "1rem",
-              color: "#FED9A7"
-            }}
-            onClick={() => attemptSponsorLogin()}
-          >
-            Sponsor Login
-          </button>
-        </div>
-      </Modal>
+        attemptSponsorLogin={attemptSponsorLogin}
+      />
     </div>
   );
 }
