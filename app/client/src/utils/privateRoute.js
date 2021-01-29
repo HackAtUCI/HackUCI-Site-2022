beda40e184c7239aef01048e93cd6575df30bcc0
Zachary Pinto <zpinto@uci.edu>
2021-01-29 11:16:55 -0800

    i hope this fixes waiver check

--- app/client/src/utils/privateRoute.js
+++ app/client/src/utils/privateRoute.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
+
+import useUser from "hooks/useUser";
 import useAuth from "hooks/useAuth";
 import useDashboardUser from "hooks/useDashboardUser";
+import useSettings from "hooks/useSettings";
 
 // wrapper for React Router <Route> which requires user to be logged in
 // specify children instead of component property
 function PrivateRoute({ children, checkWaiver }) {
   const { isLoggedIn } = useAuth();
-  const { dashboardUser } = useDashboardUser();
+  const { getCurrentUser } = useUser();
+  const { dashboardUser, updateDashboardUser } = useDashboardUser();
+  const { getPublicSettings } = useSettings();
+
+  useEffect(() => {
+    getCurrentUser()
+      .then(response => {
+        getPublicSettings()
+          .then(settingData => {
+            updateDashboardUser(response.data, settingData.data);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [getCurrentUser, getPublicSettings, updateDashboardUser]);
 
   let willRedirect = false;
 
