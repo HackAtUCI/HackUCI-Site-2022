401370200db9d233ed7833302c6bf2163400437d
Lasse Nordahl <lasseanordahl@gmail.com>
2021-01-28 22:03:07 -0800

    waiver signed

--- app/client/src/utils/privateRoute.js
+++ app/client/src/utils/privateRoute.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import useAuth from "hooks/useAuth";
+import useDashboardUser from "hooks/useDashboardUser";
 
 // wrapper for React Router <Route> which requires user to be logged in
 // specify children instead of component property
 function PrivateRoute({ children }) {
   const { isLoggedIn } = useAuth();
+  const { dashboardUser } = useDashboardUser();
   return (
     <Route
       render={({ location }) =>
-        isLoggedIn ? (
+        isLoggedIn && dashboardUser.waiverSigned ? (
           children
         ) : (
           <Redirect
