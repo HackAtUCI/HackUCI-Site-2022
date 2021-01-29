40dcfb16871473fe9d4f13c5c4755fcba0ecb0d6
Lasse Nordahl <lasseanordahl@gmail.com>
2021-01-29 14:37:57 -0800

    progr

--- app/client/src/app/views/live-expo/liveExpo.js
+++ app/client/src/app/views/live-expo/liveExpo.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react";
 
 import "./liveExpo.scss";
 
+import moment from "moment";
 import Youtube from "react-youtube";
-import { Fireflies, SponsorLoginModal } from "app/components";
+import { Fireflies } from "app/components";
+
+let OPENING_CEREMONY = "UEncaPrcn_U";
+let CLOSING_CEREMONY = "zsah7W_P4lM";
 
 export default function LiveExpo(props) {
+  let currentDate = moment();
+  let showClosingDate = moment("2010-10-20 4:30", "YYYY-MM-DD HH:mm");
+
   return (
     <div className="live-expo">
       <Fireflies fireflyCount={30} />
