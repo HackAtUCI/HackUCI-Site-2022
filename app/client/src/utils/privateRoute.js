ae6e21db2d0a0697fed08d5918e801a60527ac78
Taesung Hwang <taesungh@uci.edu>
2021-01-29 15:13:55 -0800

    set authentication to resolve before redirecting

--- app/client/src/utils/privateRoute.js
+++ app/client/src/utils/privateRoute.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect, Route } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
 
 import useUser from "hooks/useUser";
 import useAuth from "hooks/useAuth";
@@ -12,15 +13,31 @@ function PrivateRoute({ children, checkWaiver }) {
   const { isLoggedIn } = useAuth();
   const { getCurrentUser } = useUser();
 
-  let waiverSigned = false;
+  const [waiverSigned, setWaiverSigned] = useState(false);
+  const [resolved, setResolved] = useState(false);
 
-  getCurrentUser()
-    .then(response => {
-      waiverSigned = response.data.confirmation.signatureLiability !== "";
-    })
-    .catch(err => {
-      console.log("could not resolve user:", err);
-    });
+  useEffect(() => {
+    getCurrentUser()
+      .then(response => {
+        setWaiverSigned(response.data.confirmation.signatureLiability > 0);
+      })
+      .catch(err => {
+        console.log("could not resolve user:", err);
+      })
+      .then(() => {
+        // attempt to authenticate has completed
+        setResolved(true);
+      });
+  }, [getCurrentUser]);
+
+  if (checkWaiver && !resolved) {
+    return (
+      <div className="authentication">
+        <h1>Authenticating...</h1>
+        <Spinner animation="border" role="status" />
+      </div>
+    );
+  }
 
   const authorized = isLoggedIn && (!checkWaiver || waiverSigned);
 
