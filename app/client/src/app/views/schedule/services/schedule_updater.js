f9535bbaacd0adeaaa51d28b2a9552af41f0fb92
Taesung Hwang <taesungh@uci.edu>
2021-01-30 01:43:22 -0800

    fixed access control issues with axios

--- app/client/src/app/views/schedule/services/schedule_updater.js
+++ app/client/src/app/views/schedule/services/schedule_updater.js
@@ -1,6 +1,9 @@
-import axios from "axios";
+import axios_global from "axios";
 
-// this feed requires https on the client
+// access control headers for Google API do not allow 'x-access-token'
+const axios = axios_global.create();
+delete axios.defaults.headers.common["access-token"];
+
 const FEED_URL = "https://spreadsheets.google.com/feeds/cells/";
 const SPREADSHEET_KEY = "1zqFvnz32yXvwPVmnynXSKrVKos2PzcnzHvprBA9ouuk";
 const WORKSHEET = "3";
@@ -21,12 +24,8 @@ const DATA_URL =
 
 const extractCell = data => data.entry.content.$t;
 
-const axios_config = {
-  headers: { "X-Requested-With": "XMLHttpRequest" }
-};
-
 const getScheduleUpdates = () => {
-  const request = axios.get(DATA_URL, axios_config).then(response => {
+  const request = axios.get(DATA_URL).then(response => {
     // console.log(response.data.entry.content.$t);
     return JSON.parse(extractCell(response.data));
   });
