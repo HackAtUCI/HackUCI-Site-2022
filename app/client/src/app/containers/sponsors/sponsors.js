28f6d7817e57d27e58a35e37145268413d73dd6f
henryg0 <hgip@uci.edu>
2022-02-24 00:07:51 -0800

    added mage.ai to list of sponsors & updated figma to stand out

--- app/client/src/app/containers/sponsors/sponsors.js
+++ app/client/src/app/containers/sponsors/sponsors.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./sponsors.scss";
 
-export default function Sponsors({ sponsors }) {
+export default function Sponsors(props) {
   return (
     <div className="sponsors-container">
       <div className="sponsors-container__images">
         <div className="sponsors-container__images-grid">
-          {sponsors.map(sponsor => (
+          <a
+            key={props.figmaSponsor.name}
+            href={props.figmaSponsor.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="sponsors-image"
+              style={{ width: "45%" }}
+              src={props.figmaSponsor.src}
+              alt={props.figmaSponsor.name}
+            />
+          </a>
+        </div>
+        <div className="sponsors-container__images-grid">
+          {props.sponsors.map((sponsor) => (
             <a
               key={sponsor.name}
               href={sponsor.url}
